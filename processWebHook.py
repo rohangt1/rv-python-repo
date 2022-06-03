2eceef810949dd2840c4197481ef16ee396aac9c
Rohan Vishwakarma <rohangt925@gmail.com>
2022-06-03 17:02:43 +0530

    a

--- processWebHook.py
+++ processWebHook.py
@@ -15,21 +15,21 @@ def favicon():
 @app.route('/home')
 def home():
 
-    remote_url = 'https://cwac.in/init_certification_client_application/1645433053973/forms/OUTPUT11.docx'
-    # Define the local filename to save data
-    local_file = 'OUTPUT11.docx'
-    # Download remote and save locally
-    request.urlretrieve(remote_url, local_file)
+    # remote_url = 'https://cwac.in/init_certification_client_application/1645433053973/forms/OUTPUT11.docx'
+    # # Define the local filename to save data
+    # local_file = 'OUTPUT11.docx'
+    # # Download remote and save locally
+    # request.urlretrieve(remote_url, local_file)
 
-    session = ftplib.FTP('ftp.cwac.in','cwacin','$Rv01111996')
-    # convert("OUTPUT11.docx")
-    # convert("OUTPUT11.docx", "OUTPUT11.pdf")
-    doc = aw.Document("OUTPUT11.docx")
-    doc.save("OUTPUT11.pdf")
-    file = open('OUTPUT11.pdf','rb')                  # file to send
-    session.storbinary('STOR OUTPUT11.pdf', file)     # send the file
-    file.close()                                    # close file and FTP
-    session.quit()
+    # session = ftplib.FTP('ftp.cwac.in','cwacin','$Rv01111996')
+    # # convert("OUTPUT11.docx")
+    # # convert("OUTPUT11.docx", "OUTPUT11.pdf")
+    # doc = aw.Document("OUTPUT11.docx")
+    # doc.save("OUTPUT11.pdf")
+    # file = open('OUTPUT11.pdf','rb')                  # file to send
+    # session.storbinary('STOR OUTPUT11.pdf', file)     # send the file
+    # file.close()                                    # close file and FTP
+    # session.quit()
     return "Converted docx to pdf__now"
 
 if __name__ == "__main__":
